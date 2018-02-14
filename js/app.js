29703e9ba717bb05456b3f6d779680c6b8d3fedc
Emily Deep <emily.deep1@gmail.com>
2018-02-13 19:46:10 -0500

    top navigation

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//      Top Nav Mobile Menu     //
+
+function myFunction() {
+    var x = document.getElementById("myTopNav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
