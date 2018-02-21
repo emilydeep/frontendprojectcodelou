279fd8ddb78c9d03b238cff132ee86bcd3ad9cca
Emily Deep <emily.deep1@gmail.com>
2018-02-20 19:53:50 -0500

    added hero image and services

--- js/app.js
+++ js/app.js
@@ -1,10 +1,1 @@
 //      Top Nav Mobile Menu     //
-
-function myFunction() {
-    var x = document.getElementById("myTopNav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
